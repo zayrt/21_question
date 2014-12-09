7b98666efdabb862fdf9fbb7281a57da1f0f61cd
ilyas <ilyas.zelloufi@epitech.eu>
2014-12-09 17:40:54 +0100

    heroku

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,11 @@
 class PageController < ApplicationController
   def index
   end
+
+  def vote
+  	@question = Question.all
+  	if user_signed_in?
+  		@votes = Vote.where('user_id = ?', current_user.id)
+  	end
+  end
 end
